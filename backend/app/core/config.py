0621f84e00aafbd85c653b7945f25e44d2338e45
Isaac Perkins <isaacperkins987@gmail.com>
2026-07-01 17:34:07 -0700

    Added some additional app details and fixed the env path

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,29 +1,31 @@
 from pathlib import Path
-from fastapi import FastAPI
 from pydantic import SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+_ENV_PATH = Path(__file__).resolve().parent.parent.parent / ".env"
+
 
 class Settings(BaseSettings):
-    # Environment details
-    env_file = Path(__file__).resolve().parent.parent / ".env"
-
     # App details
     app_name: str = "PunchedN"
+    app_description: str = (
+        "API to assist with employee scheduling, and shift management."
+    )
+    app_version: str = "v1"
+    app_docs_url: str = "/docs"
+    app_redoc_url: str = "/redoc"
     admin_email: str
     debug: bool = False
 
     # Database settings
     mongo_db_user: str
     mongo_db_password: SecretStr
-    mongo_db_cluster: str
+    mongo_db_host: str
     mongo_db_app_name: str
 
     @property
     def mongo_db_url(self) -> str:
-        return f"mongodb+srv://{self.mongo_db_user}:{self.mongo_db_password}@{self.mongo_db_host}/?appName={self.mongo_db_app_name}"
+        return f"mongodb+srv://{self.mongo_db_user}:{self.mongo_db_password.get_secret_value()}@{self.mongo_db_host}/?appName={self.mongo_db_app_name}"
 
-    model_config = SettingsConfigDict(env_file=env_file)
-
-
-settings = Settings()
+    # Load settings from .env
+    model_config = SettingsConfigDict(env_file=_ENV_PATH)
