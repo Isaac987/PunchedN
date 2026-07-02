2a07ef32a0446b3f51710cbeab122cae8bfe1107
Isaac Perkins <isaacperkins987@gmail.com>
2026-07-02 05:58:08 -0700

    Added the get_cache function and renamed mongo_db_app_name

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from functools import lru_cache
 from pydantic import SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -22,11 +23,19 @@ class Settings(BaseSettings):
     mongo_db_user: str
     mongo_db_password: SecretStr
     mongo_db_host: str
-    mongo_db_app_name: str
+    mongo_db_name: str
 
     @property
     def mongo_db_uri(self) -> str:
-        return f"mongodb+srv://{self.mongo_db_user}:{self.mongo_db_password.get_secret_value()}@{self.mongo_db_host}/?appName={self.mongo_db_app_name}"
+        return f"mongodb+srv://{self.mongo_db_user}:{self.mongo_db_password.get_secret_value()}@{self.mongo_db_host}/?appName={self.mongo_db_name}"
 
     # Load settings from .env
     model_config = SettingsConfigDict(env_file=_ENV_PATH)
+
+
+# We are handling settings like this so we can test other settings via dependency injection.
+# lru_cache will only create the settings object once, so there is not a constant need to read the .env file.
+# https://fastapi.tiangolo.com/advanced/settings/?h=sett#creating-the-settings-only-once-with-lru-cache
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
