e0c99d9d4f3a916730aa384ef754d9a291468aeb
Isaac Perkins <isaacperkins987@gmail.com>
2026-07-02 06:27:04 -0700

    Controlling the DB connection via lifespan

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from functools import lru_cache
-from core.config import Settings
+from contextlib import asynccontextmanager
+from core.config import get_settings
+from core.database import connect_to_mongo, disconnect_from_mongo
 
 
-# We are handling settings like this so we can test other settings via dependency injection.
-# lru_cache will only create the settings object once, so there is not a constant need to read the .env file.
-# https://fastapi.tiangolo.com/advanced/settings/?h=sett#creating-the-settings-only-once-with-lru-cache
-@lru_cache
-def get_settings() -> Settings:
-    return Settings()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await connect_to_mongo()
+    yield
+    await disconnect_from_mongo()
 
 
 app = FastAPI(
@@ -18,6 +18,7 @@ app = FastAPI(
     version=get_settings().app_version,
     docs_url=get_settings().app_docs_url,
     redoc_url=get_settings().app_redoc_url,
+    lifespan=lifespan,
 )
 
 # TODO: Define a list of origins
